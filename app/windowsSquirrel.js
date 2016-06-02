fb85c6d345b15b4ec215aff6a17ab63a504c0295
Lee Walker <lee.walker@commontime.com>
2016-06-02 18:16:09 +0100

    changed the windows squirrel to work!

--- app/windowsSquirrel.js
+++ app/windowsSquirrel.js
@@ -12,7 +12,7 @@ module.exports = function () {
     const exeName = process.execPath;
     const debugFile = path.resolve(rootAtomFolder, "init.txt");
 
-    fs.writeFileSync(debugFile, '1');
+    fs.appendFileSync(debugFile, '1');
 
     const spawn = function(command, args) {
         let spawnedProcess, error;
@@ -32,21 +32,22 @@ module.exports = function () {
 
     function uninstallWindows () {
         fs.appendFileSync(debugFile, '5');
+		
         regedit.deleteKey([
-            "HKEY_CLASSES_ROOT\\.mdesign",
-            "HKEY_CLASSES_ROOT\\.mdesign\\DefaultIcon",
-            "HKEY_CLASSES_ROOT\\.mdesign\\Shell",
-            "HKEY_CLASSES_ROOT\\.mdesign\\Shell\\Open",
-            "HKEY_CLASSES_ROOT\\.mdesign\\Shell\\Open\\command",
-            "HKEY_CLASSES_ROOT\\mdesign",
-            "HKEY_CLASSES_ROOT\\mdesign\\DefaultIcon",
-            "HKEY_CLASSES_ROOT\\mdesign\\Shell",
-            "HKEY_CLASSES_ROOT\\mdesign\\Shell\\Open",
-            "HKEY_CLASSES_ROOT\\mdesign\\Shell\\Open\\command"
+            "HKCR\\.mdesign",
+            "HKCR\\.mdesign\\DefaultIcon",
+            "HKCR\\.mdesign\\Shell",
+            "HKCR\\.mdesign\\Shell\\Open",
+            "HKCR\\.mdesign\\Shell\\Open\\command",
+            "HKCR\\mdesign",
+            "HKCR\\mdesign\\DefaultIcon",
+            "HKCR\\mdesign\\Shell",
+            "HKCR\\mdesign\\Shell\\Open",
+            "HKCR\\mdesign\\Shell\\Open\\command"
         ], function(err) {
             console.error("Error createing registry keys", err);
         });
-
+        
         // Remove desktop and start menu shortcuts
         var removeChildProcess = spawnUpdate(['--removeShortcut=' + exeName]);
         removeChildProcess.on("close", function () {
@@ -56,75 +57,84 @@ module.exports = function () {
 
     function installWindows() {
         fs.appendFileSync(debugFile, '4');
+        
         regedit.createKey([
-            "HKEY_CLASSES_ROOT\\.mdesign",
-            "HKEY_CLASSES_ROOT\\.mdesign\\DefaultIcon",
-            "HKEY_CLASSES_ROOT\\.mdesign\\Shell",
-            "HKEY_CLASSES_ROOT\\.mdesign\\Shell\\Open",
-            "HKEY_CLASSES_ROOT\\.mdesign\\Shell\\Open\\command",
-            "HKEY_CLASSES_ROOT\\mdesign",
-            "HKEY_CLASSES_ROOT\\mdesign\\DefaultIcon",
-            "HKEY_CLASSES_ROOT\\mdesign\\Shell",
-            "HKEY_CLASSES_ROOT\\mdesign\\Shell\\Open",
-            "HKEY_CLASSES_ROOT\\mdesign\\Shell\\Open\\command"
+            "HKCR\\.mdesign",
+            "HKCR\\.mdesign\\DefaultIcon",
+            "HKCR\\.mdesign\\Shell",
+            "HKCR\\.mdesign\\Shell\\Open",
+            "HKCR\\.mdesign\\Shell\\Open\\command",
+            "HKCR\\mdesign",
+            "HKCR\\mdesign\\DefaultIcon",
+            "HKCR\\mdesign\\Shell",
+            "HKCR\\mdesign\\Shell\\Open",
+            "HKCR\\mdesign\\Shell\\Open\\command"
         ], function(err) {
-            console.error("Error createing registry keys", err);
+			if(err) {				
+				console.error("Error createing registry keys", err);
+				fs.appendFileSync(debugFile, '\r\n7' + err);
+			} else {
+				regedit.putValue({
+					"HKCR\\.mdesign": {
+						'@': {
+							value: ".mdesign",
+							type: 'REG_DEFAULT'
+						},
+						"mDesign File Association": {
+							value: '',
+							type: 'REG_SZ'
+						}
+					},
+					"HKCR\\.mdesign\\DefaultIcon": {
+						'@': {
+							value: process.execPath,
+							type: 'REG_DEFAULT'
+						}
+					},
+					"HKCR\\.mdesign\\Shell\\Open\\command": {
+						'@': {
+							value: process.execPath,
+							type: 'REG_DEFAULT'
+						}
+					},
+					"HKCR\\mdesign": {
+						'@': {
+							value: "URL:mdesign Protocol",
+							type: 'REG_DEFAULT'
+						},
+						'URL Protocol': {
+							value: '',
+							type: 'REG_SZ'
+						}
+					},
+					"HKCR\\mdesign\\DefaultIcon": {
+						'@': {
+							value: process.execPath,
+							type: 'REG_DEFAULT'
+						}
+					},
+					"HKCR\\mdesign\\Shell\\Open\\command": {
+						'@': {
+							value: process.execPath,
+							type: 'REG_DEFAULT'
+						}
+					}
+				}, function(err) {
+					if(err) {
+						console.error("Error saving value to keys", err);
+						fs.appendFileSync(debugFile, '\r\n8' + err);
+					}
+				});
+			}
         });
 
-        regedit.putValue({
-            "HKEY_CLASSES_ROOT\\.mdesign": {
-                '@': {
-                    value: ".mdesign",
-                    type: 'REG_SZ'
-                },
-                "mDesign File Association": {
-                    value: '',
-                    type: 'REG_SZ'
-                }
-            },
-            "HKEY_CLASSES_ROOT\\.mdesign\\DefaultIcon": {
-                '@': {
-                    value: process.execPath,
-                    type: 'REG_SZ'
-                }
-            },
-            "HKEY_CLASSES_ROOT\\.mdesign\\Shell\\Open\\command": {
-                '@': {
-                    value: process.execPath,
-                    type: 'REG_SZ'
-                }
-            },
-            "HKEY_CLASSES_ROOT\\mdesign": {
-                '@': {
-                    value: "URL:mdesign Protocol",
-                    type: 'REG_SZ'
-                },
-                'URL Protocol': {
-                    value: '',
-                    type: 'REG_SZ'
-                }
-            },
-            "HKEY_CLASSES_ROOT\\mdesign\\DefaultIcon": {
-                '@': {
-                    value: process.execPath,
-                    type: 'REG_SZ'
-                }
-            },
-            "HKEY_CLASSES_ROOT\\mdesign\\Shell\\Open\\command": {
-                '@': {
-                    value: process.execPath,
-                    type: 'REG_SZ'
-                }
-            }
-        }, function(err) {
-            console.error("Error saving value to keys", err);
-        });
+        
     }
 
     function updatedWindows () {
         fs.appendFileSync(debugFile, '6');
         // Install desktop and start menu shortcuts
-        var createChildProcess = spawnUpdate(['--createShortcut=' + exeName, "Desktop"]);
+        var createChildProcess = spawnUpdate(['--createShortcut=' + exeName]);
         createChildProcess.on("close", function () {
             app.quit();
         });
@@ -132,12 +142,9 @@ module.exports = function () {
 
     function handleSquirrelEvent() {
         fs.appendFileSync(debugFile, '2');
-        if (process.argv.length !== 1) {
-            fs.appendFileSync(debugFile, '3');
-            return false;
-        }
 
         const squirrelEvent = process.argv[1];
+		var handled = false;
         switch (squirrelEvent) {
             case '--squirrel-install':
                 installWindows();
@@ -146,28 +153,25 @@ module.exports = function () {
                 // - Add your .exe to the PATH
                 // - Write to the registry for things like file associations and
                 //   explorer context menus
+				handled = true;
                 updatedWindows();
                 break;
             case '--squirrel-uninstall':
                 // Undo anything you did in the --squirrel-install and
                 // --squirrel-updated handlers
+				handled = true;
                 uninstallWindows();
                 break;
             case '--squirrel-obsolete':
                 // This is called on the outgoing version of your app before
                 // we update to the new version - it's the opposite of
                 // --squirrel-updated
-
+				handled = true;
                 app.quit();
                 break;
         }
-        return true;
+        return handled;
     }
 
-    // this should be placed at top of main.js to handle setup events quickly
-    if (handleSquirrelEvent()) {
-        fs.appendFileSync(debugFile, 'end');
-        // squirrel event handled and app will exit in 1000ms, so don't do anything else
-        return true;
-    }
+    return handleSquirrelEvent();
 };
